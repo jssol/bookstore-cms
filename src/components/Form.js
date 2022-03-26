1c5c24efa673db536c8f36dedd22c24c3d916ed1
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-26 21:08:35 +0200

    Change placeholder to Author

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ const Form = () => (
     <h2 className="text-slate-500 text-xl font-bold mb-2">ADD NEW BOOK</h2>
     <form className="w-full h-10 flex items-stretch justfiy-between">
       <input className="w-3/5 p-3 rounded-md text-slate-600 transition-all h-full block outline-1 outline-blue-100 focus:outline-blue-300" type="text" placeholder="Book title" />
-      <input className="w-1/3 p-3 rounded-md text-slate-600 transition-all h-full block ml-4 outline-blue-100 focus:outline-blue-300" type="" placeholder="Category" />
+      <input className="w-1/3 p-3 rounded-md text-slate-600 transition-all h-full block ml-4 outline-blue-100 focus:outline-blue-300" type="" placeholder="Author" />
       <button className="w-1/5 p-3 flex items-center justify-center rounded-md h-full block bg-blue-600 text-white font-semibold ml-4" type="submit">ADD BOOK</button>
     </form>
   </section>
