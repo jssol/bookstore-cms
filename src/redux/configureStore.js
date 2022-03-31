563b842d6702d5f667317e4d6921882c6e953ddf
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-31 22:18:19 +0200

    Create store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from 'redux';
+import categoriesReducer from './categories/categories';
+import booksReducer from './books/books';
+
+const rootReducer = combineReducers({
+  books: booksReducer,
+  categories: categoriesReducer
+});
+
+const store = createStore(rootReducer);
+
+export default store;
