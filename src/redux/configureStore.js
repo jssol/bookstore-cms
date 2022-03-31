63d528913da246bbf8071d20abbb301b3bd4885e
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-31 23:59:06 +0200

    Revert "Remove addtional reqs"
    This reverts commit e3794faef5165db17d993e0be434026782fde6fd.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import categoriesReducer from './categories/categories';
 import booksReducer from './books/books';
+import visibilityReducer from './filter/filter';
 
 const rootReducer = combineReducers({
   books: booksReducer,
   categories: categoriesReducer,
+  visibility: visibilityReducer,
 });
 
 const store = createStore(rootReducer);
