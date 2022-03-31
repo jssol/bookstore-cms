e3794faef5165db17d993e0be434026782fde6fd
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-31 23:42:02 +0200

    Remove addtional reqs

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import categoriesReducer from './categories/categories';
 import booksReducer from './books/books';
-import visibilityReducer from './filter/filter';
 
 const rootReducer = combineReducers({
   books: booksReducer,
   categories: categoriesReducer,
-  visibility: visibilityReducer,
 });
 
 const store = createStore(rootReducer);
