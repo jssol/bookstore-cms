2a1b1abc8ccc5c5a3e66c09063dbd2d38a5477b7
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-01 14:35:05 +0200

    Consume the books state props

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,19 @@
+import React from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
 import Form from './Form';
 import Book from './Book';
 
-const Books = () => (
-  <main className="w-full h-screen overflow-hidden bg-gray-100 flex flex-col items-center">
-    <section className="mt-5 max-h-3/5 w-10/12 flex flex-col items-stretch gap-y-2 overflow-y-auto">
-      <Book />
-      <Book />
-    </section>
-    <Form />
-  </main>
-);
+const Books = () => {
+  const books = useSelector((state) => state.books, shallowEqual);
+
+  return (
+    <main className="w-full h-screen overflow-hidden bg-gray-100 flex flex-col items-center">
+      <section className="mt-5 max-h-3/5 w-10/12 flex flex-col items-stretch gap-y-2 overflow-y-auto">
+        {books.map((book) => <Book book={book} key={book.id} />)}
+      </section>
+      <Form />
+    </main>
+  );
+};
 
 export default Books;
