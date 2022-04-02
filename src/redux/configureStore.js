55c1f6e9a0f7c8922d225e43b3d16c29e1fe5fda
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-02 15:50:50 +0200

    Apply a thunk to the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import categoriesReducer from './categories/categories';
 import booksReducer from './books/books';
 import visibilityReducer from './filter/filter';
@@ -9,6 +10,6 @@ const rootReducer = combineReducers({
   visibility: visibilityReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
